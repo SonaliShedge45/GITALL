7c265a8151021e4ad4db9802191c27e336ed68a5
SonaliShedge45 <sonalishedge770@gmail.com>
2024-07-29 21:27:53 +0530

    Print User Names

---
+++ src/GitPro/Array.java
@@ -0,0 +1,15 @@
+package GitPro;
+
+public class Array {
+
+	public static void main(String[] args) {
+		String[] s = {"Sonali", "Ishika", "Meera", "Ritika", "Sai"};
+		for (int i = 0; i<s.length; i++) {
+		System.out.println(s[i]);
+			
+		}
+		
+
+	}
+
+}
